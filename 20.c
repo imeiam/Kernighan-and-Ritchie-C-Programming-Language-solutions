2bc2bfb95404a48122394ad46d3d71533d6cad63
Akil Adeshwar <akil.adeshwar@gmail.com>
2016-06-02 17:30:52 +0530

    Completed till 20

--- 20.c
+++ 20.c
@@ -1,20 +1,27 @@
 #include<stdio.h>
 #include<string.h>
 
+
+	
+
 /*
-	Write a function reverse(s) that reverses the character string s . Use it to write a program that reverses its input a line at a time.
+   Write a program detab that replaces tabs in the input with the proper number of blanks to space to the next tab stop.
+   Assume a fixed set of tab stops, say every n columns.
 
 */
 
 #define MAXLENGTH_LINE 100
 
+#define TAB_SIZE 7 // equivalent to 7 spaces. Change tab sizes from 7 to 1 for testing.
+
 
 int main(void){
 
-	FILE *fp = fopen("t19.txt","r");
+	FILE *fp = fopen("t20.txt","r");
 	char str[100];
 	int line_max_len = MAXLENGTH_LINE;
 	int len,i;
+	char *p = NULL;
 	char c;
 	if(fp!=NULL){
 
@@ -24,13 +31,16 @@ int main(void){
 				break;
 			if(len == 0 )
 				continue;
-			// String reverse logic
-			for(i=0;i<len/2;i++){
-				c = str[i];
-				str[i]= str[len-1-i];
-				str[len-1-i] = c;
-			}	
-			puts(str);
+			p = str;
+			while((c=*p)){
+				if(c == '\t')
+					for(i=0;i<TAB_SIZE;i++)
+						putchar(' ');
+				else
+					putchar(c);
+				p++;
+			}
+			putchar('\n');
 		}
 		fclose(fp);
 	}
