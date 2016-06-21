b8912ea1b85698c38d34ae364a8def2a1ed840bb
Akil Adeshwar <akil.adeshwar@gmail.com>
2016-06-21 16:37:05 +0530

    Completed question 5

---
+++ 5.c
@@ -0,0 +1,135 @@
+/*
+   Rewrite the postfix calculator of Chapter 4 to use scanf and/or sscanf to do the input and number conversion.
+   */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+#define MAXOP 100
+#define NUMBER 0
+#define MAXVAL 100
+#define BUFSIZE 100
+
+int getop(char *);
+void push(double);
+double pop(void);
+int getch(void);
+void ungetch(int);
+void viewstack(void);
+
+int bufp = 0;
+char buf[BUFSIZE];
+int sp = 0;
+double val[MAXVAL];
+
+
+void push(double f)
+{
+	if(sp < MAXVAL)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can't push %g\n", f);
+}
+
+double pop(void)
+{
+	if(sp > 0)
+		return val[--sp];
+	else
+	{
+		printf("error: stack empty\n");
+		exit(1);
+	}
+}
+
+int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+	if(bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
+
+int getop(char *s)
+{
+	int i, c, d;
+	while((s[0] = c = getch()) == ' ' || c == '\t');
+	s[1] = '\0';
+	if(!isdigit(c) && c != '.' && c != '-')
+		return c;
+	if(c == '-')
+	{
+		d = getch();
+		if(d == ' ')
+			return c;
+		else
+			ungetch(d);
+	}
+	i = 0;
+	if(isdigit(c) || c == '-')
+		while(isdigit(s[++i] = c = getch()));
+	if(c == '.')
+		while(isdigit(s[++i] = c = getch()));
+	s[i] = '\0';
+	if(c != EOF)
+		ungetch(c);
+	return NUMBER;
+}
+
+
+int main(void)
+{
+	int type;
+	double op2;
+	double num;
+	char s[MAXOP];
+
+	while((type = getop(s)) != EOF)
+	{
+		switch(type)
+		{
+			case NUMBER:
+				sscanf(s,"%lf",&num);
+				push(num);
+				break;
+			case '+':
+				push(pop() + pop());
+				break;
+			case '*':
+				push(pop() * pop());
+				break;
+			case '-':
+				op2 = pop();
+				push(pop() - op2);
+				break;
+			case '/':
+				op2 = pop();
+				if(op2 != 0.0)
+					push(pop() / op2);
+				else
+					printf("error: zero divisor\n");
+				break;
+			case '%':
+				op2 = pop();
+				if(op2 != 0.0)
+					push(fmod(pop(), op2));
+				else
+					printf("error: division by zero\n");
+				break;
+			case '\n':
+				printf("\t%.8g\n", pop());
+				break;
+			default:
+				printf("error: unknown command %s\n", s);
+				break;
+		}
+	}
+	return 0;
+}
