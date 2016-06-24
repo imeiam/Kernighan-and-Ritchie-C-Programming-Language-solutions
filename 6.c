0b677cafc7484aa38f9a1c101b75d9437872f991
Akil Adeshwar <akil.adeshwar@gmail.com>
2016-06-24 17:38:29 +0530

    Update q6

--- 6.c
+++ 6.c
@@ -1,3 +1,9 @@
+/*
+   The standard library function calloc(n,size) 
+   returns a pointer to n objects of size size ,
+   with the storage initialized to zero. Write calloc , by calling malloc or by modifying it.
+   */
+
 #include<stdio.h>
 #include<string.h>
 
